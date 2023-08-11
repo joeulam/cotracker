4a6540f89bf4008d9e62d35d5c11bc2c5a2ae53c
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-08-10 22:33:50 -0400

    Add skeleton component files

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import { FaBars } from 'react-icons/fa'; // Mobile navbar menu icon
+import { GrClose } from 'react-icons/gr'; // Mobile navbar menu close icon
+
+const Footer = () => {
+    return (
+        <footer>
+            
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ components/NavBar.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import { FaBars } from 'react-icons/fa'; // Mobile navbar menu icon
+import { GrClose } from 'react-icons/gr'; // Mobile navbar menu close icon
+
+const NavBar = ({page}) => {
+    return (
+        <div className='top-0 left-0 w-full z-[999]'>
+            
+        </div>
+    );
+}
+
+export default NavBar;
