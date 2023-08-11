1ae10523696f04ed6ac71d7d7deea11bc047d605
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-08-10 22:33:23 -0400

    Migrate app to TypeScript, metadata changes

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import { Comfortaa, Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+const comfortaa = Comfortaa({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'CoTrackers',
+  description: 'Finance Tracker built with Next.js, React, and Tailwind.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} bg-[#E7E7E7]`}>{children}</body>
+    </html>
+  )
+}
--- app/loginPage/page.tsx
+++ app/loginPage/page.tsx
@@ -5,11 +5,11 @@ export default function login(){
                 <p>Login</p>
                 <form>
                     <h3>Email:</h3>
-                    <input type="email" class="outline-2 outline-black outline"></input>
+                    <input type="email" className="outline-2 outline-black outline"></input>
                     <h3>Password</h3>
-                    <input type="password" class="outline-2 outline-black outline"></input>
+                    <input type="password" className="outline-2 outline-black outline"></input>
                     <br/>
-                    <button class="outline-2 outline-black outline mt-5">Login</button>
+                    <button className="outline-2 outline-black outline mt-5">Login</button>
                 </form>
             </div>
         </main>
