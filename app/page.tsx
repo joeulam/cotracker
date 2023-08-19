8be01ed8622cf36867dc736a6309d327fd7caef0
OmnipotentBlade <91920612+OmnipotentBlade@users.noreply.github.com>
2023-08-19 14:19:30 -0400

    Fix README, main page navbar import

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import NavBar from '../components/Navbar';
+import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
 import background from '/assets/background-home.jpg';
 
