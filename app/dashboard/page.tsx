e278856d1ec8b60f3cb5fc517dc9d70c74360e4c
Joey Lam <joeulam345@gmail.com>
2024-01-05 02:01:00 -0500

    Added MUI Started dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,63 @@
 export default function dashboard(){
     return(
         <main>
-            THIS IS THE DASHBOARD
+            <div>
+                <div>
+                    LOGO
+                </div>
+                <div>
+                    Dashboard
+                </div>
+                <div>
+                    Finance
+                </div>
+                <div>
+                    Calendar
+                </div>
+                <div>
+                    Wallet
+                </div>
+                <div>
+                    Setting
+                </div>
+                <div>
+                    Log Out
+                </div>
+            </div>
+
+
+            <div>
+                <h1>Dashboard</h1>
+                <p>Date</p>
+            </div>
+
+
+            <div>
+                <h2>Your balance:</h2>
+                <h1>BALANCE</h1>
+                <h3>CARD INFO</h3>
+            </div>
+
+            <div>
+                <div>
+                    <select name="date" id="dates">
+                        <option value="week">Last week</option>
+                        <option value="month">Last Month</option>
+                        <option value="month6">Last six month</option>
+                        <option value="year">Last year</option>
+                    </select>
+                </div>
+                <div>
+                    <h3>Transaction</h3>
+                    <p></p>
+                </div>
+                <div>
+                    <h3>Total Spent</h3>
+                    <p></p>
+                </div>
+            </div>
+
+
         </main>
     )
 }
