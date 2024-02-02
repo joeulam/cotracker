876dc1801ea6f2bfce95c420e914a818d9bb18fa
joeulam <joeulam345@gmail.com>
2024-02-02 17:11:33 -0500

    Working on new transactions

--- components/NewTransaction.tsx
+++ components/NewTransaction.tsx
@@ -14,9 +14,21 @@ import Button from '@mui/material/Button';
 import React from 'react';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import MenuItem from '@mui/material/MenuItem';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+
+
+
 
 const NewTransaction = () => {
 
+  // Allows for the snackbar to popup on the bottom left //
   const [open, setOpen] = React.useState(false);
 
   const handleClick = () => {
@@ -30,7 +42,6 @@ const NewTransaction = () => {
 
     setOpen(false);
   };
-
   const action = (
     <React.Fragment>
       <IconButton
@@ -43,37 +54,100 @@ const NewTransaction = () => {
       </IconButton>
     </React.Fragment>
   );
+// ------------------------------------ // 
 
 
+  // List the choices for a multi line tag // 
+
+  const names = [ 
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
+    'Ralph Hubbard',
+    'Omar Alexander',
+    'Carlos Abbott',
+    'Miriam Wagner',
+    'Bradley Wilkerson',
+    'Virginia Andrews',
+    'Kelly Snyder',
+  ]; // Change it from hard code to getting it from firebase -Joey // 
+
+  const [personName, setPersonName] = React.useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      // On autofill we get a stringified value.
+      typeof value === 'string' ? value.split(',') : value,
+    );
+  };
+// ------------------------------------------------------------------------- //
     return (
-        <div>
+        <div className="relative mr-50 bg-white w-[50vw] h-[50vh] justify-center	">
             <h1>New Transaction</h1>
+
             <div>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DateField label="Basic date field" />
+                    <DateField label="Date" />
                 </LocalizationProvider>
             </div>
+
             <div>
-            <FormControl fullWidth sx={{ m: 1 }} variant="filled">
-                <InputLabel htmlFor="filled-adornment-amount">Amount</InputLabel>
-                <FilledInput
-                    id="filled-adornment-amount"
-                    startAdornment={<InputAdornment position="start">$</InputAdornment>}
+              <FormControl  sx={{ m: 1 }} variant="filled">
+                  <InputLabel htmlFor="filled-adornment-amount">Amount</InputLabel>
+                  <FilledInput
+                      id="filled-adornment-amount"
+                      startAdornment={<InputAdornment position="start">$</InputAdornment>}
+                  />
+              </FormControl>
+            </div>
+
+            <div>
+              <FormControl>
+                <FormLabel id="demo-row-radio-buttons-group-label"></FormLabel>
+                <RadioGroup
+                  row
+                  aria-labelledby="demo-row-radio-buttons-group-label"
+                  name="row-radio-buttons-group"
+                >
+                  <FormControlLabel value="1" control={<Radio />} label="Income" />
+                  <FormControlLabel value="-1" control={<Radio />} label="Expense" />
+                </RadioGroup>
+              </FormControl>
+            </div>
+
+            <div>
+                <FormControl sx={{ m: 1, width: 300 }}>
+                  <InputLabel id="demo-multiple-name-label">Labels</InputLabel>
+                    <Select
+                      labelId="demo-multiple-name-label"
+                      id="demo-multiple-name"
+                      multiple
+                      value={personName}
+                      onChange={handleChange}
+                      input={<OutlinedInput label="Name" />}
+                    >
+                      {names.map((name) => (
+                        <MenuItem
+                          key={name}
+                          value={name}
+                        >
+                          {name}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                </FormControl>
+            </div>
+            <div>
+                <TextField
+                  id="filled-multiline-static"
+                  label="Comments"
+                  multiline
+                  rows={4}
+                  variant="filled"
                 />
-            </FormControl>
-            </div>
-            <div>
-                <h3>Tags</h3>
-                <select multiple>
-                    <option>Home</option>
-                    <option>Income</option>
-                    <option>Healthcare</option>
-                    <option>Food</option>
-                </select>
-            </div>
-            <div>
-                <h3>Comments</h3>
-                <input type='text'></input>
             </div>
             
 
@@ -91,7 +165,7 @@ const NewTransaction = () => {
 
             />
 
-
+            
         </div>
     );
 }
