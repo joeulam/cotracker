6122fcd822ac51acabb595e79b5d4443acc63c62
joeulam <joeulam345@gmail.com>
2024-10-11 14:40:35 -0400

    Added a function to grab the recent transactions

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,15 @@ import WindowIcon from "@mui/icons-material/Window";
 import { format } from "date-fns";
 import { getAuth } from "firebase/auth";
 import { useRouter } from "next/navigation";
-import { doc, setDoc, getDoc, collection, updateDoc, increment } from "firebase/firestore";
+import {
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  collection,
+  updateDoc,
+  increment,
+} from "firebase/firestore";
 import { db } from "../../firebase_api/firebaseConfig";
 import { useEffect, useState } from "react";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
@@ -34,13 +42,14 @@ import {
   useCombobox,
   PillsInput,
   Pill,
+  Container,
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import "@mantine/core/styles.css";
 import "@mantine/dates/styles.css";
-import { notifications } from '@mantine/notifications';
-import { Notifications } from '@mantine/notifications';
-import '@mantine/notifications/styles.css';
+import { notifications } from "@mantine/notifications";
+import { Notifications } from "@mantine/notifications";
+import "@mantine/notifications/styles.css";
 
 export default function Dashboard() {
   const router = useRouter();
@@ -68,6 +77,19 @@ export default function Dashboard() {
     }
   }
 
+  async function transactionGrab() {
+    const auth = getAuth();
+    const user = auth.currentUser;
+    if (user) {
+      const uid = user.uid;
+      const docRef = await (collection(db, "user", uid, "newTransactions"));
+      const docSnap = await getDocs(docRef);
+      return docSnap
+    } else {
+      Kick();
+    }
+  }
+
   function Kick() {
     router.push("/login");
   }
@@ -83,57 +105,55 @@ export default function Dashboard() {
   async function submitNewTransaction() {
     setVisible.open();
     try {
-			if(transactionName != "" && cost != ""){
-				await setDoc(
-					doc(
-						collection(doc(db, "user", uid), "newTransactions"),
-						dates.toString()
-					),
-					{
-						date: dates,
-						transactionName: transactionName,
-						tags: value, // Pass your tags array here
-						cost: cost,
-					}
-				);
-				await updateDoc(doc(db, "user", uid),{
-					total_spent: increment(Number(cost)),
-					transaction: increment(1),
-				})
-				console.log("Document successfully written!");
-				setOpened.toggle();
-				setVisible.close();
-				notifications.show({
-					title: 'Transaction recorded',
-					message: null,
-					color: 'green'
-				})    
-				dates = null;
-				transactionName = null;
-				cost = null;
-				value = null;
-				// TODO CLEAR variables after submitting
-				fetchData()
-			}
-			else{
-				setVisible.close();
-				notifications.show({
-					title: 'Name and cost cannot be blank',
-					message: null,
-					color: 'red'
-				}) 
-			}
-		}
-		catch (error) {
-			console.error("Error writing document: ", error);
-			setOpened.toggle();
-			setVisible.close();
-			notifications.show({
-				title: 'Something went wrong',
-				message: error.toString(),
-				color: 'red'
-			})    
-		}
+      if (transactionName != "" && cost != "") {
+        await setDoc(
+          doc(
+            collection(doc(db, "user", uid), "newTransactions"),
+            dates.toString()
+          ),
+          {
+            date: dates,
+            transactionName: transactionName,
+            tags: value, // Pass your tags array here
+            cost: cost,
+          }
+        );
+        await updateDoc(doc(db, "user", uid), {
+          total_spent: increment(Number(cost)),
+          transaction: increment(1),
+        });
+        console.log("Document successfully written!");
+        setOpened.toggle();
+        setVisible.close();
+        notifications.show({
+          title: "Transaction recorded",
+          message: null,
+          color: "green",
+        });
+        dates = null;
+        transactionName = null;
+        cost = null;
+        value = null;
+        // TODO CLEAR variables after submitting
+        fetchData();
+      } else {
+        setVisible.close();
+        notifications.show({
+          title: "Name and cost cannot be blank",
+          message: null,
+          color: "red",
+        });
+      }
+    } catch (error) {
+      console.error("Error writing document: ", error);
+      setOpened.toggle();
+      setVisible.close();
+      notifications.show({
+        title: "Something went wrong",
+        message: error.toString(),
+        color: "red",
+      });
+    }
   }
 
   const [balance, setBalance] = useState<number | null>(null);
@@ -149,16 +169,19 @@ export default function Dashboard() {
   const [visible, setVisible] = useDisclosure(false); // Loading overlay
   const [opened, setOpened] = useDisclosure(false); // Add new transaction card
   const [tags, setTags] = useState<string[] | null>([]); // All tags
+  const [transactionCards, settransactionCards] = useState<string[] | null>([]); // All tags
 
-	async function fetchData() {
-		const data = await dataGrab();
-		const myInstance = new Pdata(data);
-		setBalance(myInstance.getBalance());
-		setUid(myInstance.getUid());
-		setTotal(myInstance.getTotalS());
-		setTranscation(myInstance.getTransaction());
-		setTags(myInstance.getTags());
-	}
+  async function fetchData() {
+    const data = await dataGrab();
+    const myInstance = new Pdata(data);
+    setBalance(myInstance.getBalance());
+    setUid(myInstance.getUid());
+    setTotal(myInstance.getTotalS());
+    setTranscation(myInstance.getTransaction());
+    setTags(myInstance.getTags());
+    const transactionData = await transactionGrab();
+    console.log(transactionData)
+  }
   // Fetch balance when the component mounts
   useEffect(() => {
     fetchData();
@@ -199,105 +222,132 @@ export default function Dashboard() {
       </Combobox.Option>
     ));
 
+  const recentTransactionCard = transactionCards.map((entry) => (
+    <Container>
+      
+    </Container>
+  ))
+
+
   return (
     <MantineProvider defaultColorScheme="light">
-      <div className="flex">
-        <div className="h-[100vh] bg-[#4D4D4D] w-[15vw]">
-          <div className="h-[10vh] w-[15vw]">
-            <div className="flex items-center justify-center mt-[10%]">
-              <Image src={logo} height={38} alt="CoTrackers" />
+      <div className="">
+        <div className="flex flex-row">
+          <div className="h-[100vh] bg-[#4D4D4D] w-[15vw] basis-1/6">
+            <div className="h-[10vh] w-[15vw]">
+              <div className="flex items-center justify-center mt-[10%]">
+                <Image src={logo} height={38} alt="CoTrackers" />
+              </div>
+            </div>
+            <div className="ml-[1vw]">
+              <div className="ml-[-1vw] bg-[#6E6E6E] border-2">
+                <div className="ml-[1vw] h-[8vh] flex items-center left ">
+                  <WindowIcon className="mr-[1vw]" sx={{ color: "white" }} />
+                  <div className="text-white">Dashboard</div>
+                </div>
+              </div>
+
+              <div className="h-[8vh] flex items-center ">
+                <TrendingUpIcon className="mr-[1vw]" sx={{ color: "white" }} />
+                <div className="text-white">
+                  <a href="/finances">Finance</a>
+                </div>
+              </div>
+              <div className="h-[8vh] flex items-center ">
+                <CalendarTodayIcon
+                  className="mr-[1vw]"
+                  sx={{ color: "white" }}
+                />
+                <div className="text-white">Calendar</div>
+              </div>
+              <div className="h-[8vh] flex items-center ">
+                <AccountBalanceWalletIcon
+                  className="mr-[1vw]"
+                  sx={{ color: "white" }}
+                />
+                <div className="text-white">Wallet</div>
+              </div>
+              <div className="h-[8vh] flex items-center ">
+                <SettingsIcon className="mr-[1vw]" sx={{ color: "white" }} />
+                <div className="text-white">Setting</div>
+              </div>
+              <div className="text-white h-[8vh] flex items-center ">
+                <div className="text-wrap">
+                  Logged in as {uid !== null ? uid : "Loading..."}
+                </div>
+              </div>
+              <div className="h-[8vh] flex items-center ">
+                <LogoutIcon className="mr-[1vw]" sx={{ color: "white" }} />
+                <div className="text-white">Log Out</div>
+              </div>
             </div>
           </div>
-          <div className="ml-[1vw]">
-            <div className="ml-[-1vw] bg-[#6E6E6E] border-2">
-              <div className="ml-[1vw] h-[8vh] flex items-center left ">
-                <WindowIcon className="mr-[1vw]" sx={{ color: "white" }} />
-                <div className="text-white">Dashboard</div>
-              </div>
-            </div>
 
-            <div className="h-[8vh] flex items-center ">
-              <TrendingUpIcon className="mr-[1vw]" sx={{ color: "white" }} />
-              <div className="text-white">
-                <a href="/finances">Finance</a>
+          <div className="ml-[3vw] basis-4/6">
+              <div>
+                <h1 className="text-[48px]">Dashboard</h1>
+                <p>{date()}</p>
               </div>
-            </div>
-            <div className="h-[8vh] flex items-center ">
-              <CalendarTodayIcon className="mr-[1vw]" sx={{ color: "white" }} />
-              <div className="text-white">Calendar</div>
-            </div>
-            <div className="h-[8vh] flex items-center ">
-              <AccountBalanceWalletIcon
-                className="mr-[1vw]"
-                sx={{ color: "white" }}
-              />
-              <div className="text-white">Wallet</div>
-            </div>
-            <div className="h-[8vh] flex items-center ">
-              <SettingsIcon className="mr-[1vw]" sx={{ color: "white" }} />
-              <div className="text-white">Setting</div>
-            </div>
-            <div className="text-white h-[8vh] flex items-center ">
-              <div className="text-wrap">
-                Logged in as {uid !== null ? uid : "Loading..."}
+
+              <div className="bg-white pr-[20vw] pl-[2vw] pt-[2vh] pb-[2vh] rounded-[15px] mt-[4vh]">
+                <h2 className="text-[30px]">Your Budget Left:</h2>
+                <h1 className="text-[30px]">
+                  ${balance !== null ? balance : "Loading..."}
+                </h1>
               </div>
-            </div>
-            <div className="h-[8vh] flex items-center ">
-              <LogoutIcon className="mr-[1vw]" sx={{ color: "white" }} />
-              <div className="text-white">Log Out</div>
-            </div>
+
+              <div className="flex mt-[7vh]">
+                <div className="bg-white rounded-[15px] p-8 mr-[1vw]">
+                  <select name="date" id="dates" className="items-center">
+                    <option value="week">Last week</option>
+                    <option value="month">Last Month</option>
+                    <option value="month6">Last six month</option>
+                    <option value="year">Last year</option>
+                  </select>
+                </div>
+                <div className="bg-white rounded-[15px] p-8 mr-[1vw]">
+                  <h3>Transaction</h3>
+                  <p>{trans !== null ? trans : "Loading..."}</p>
+                </div>
+                <div className="bg-white rounded-[15px] p-8">
+                  <h3>Total Spent</h3>
+                  <p>{total !== null ? total : "Loading..."}</p>
+                </div>
+              </div>
+
+              <div className="flex mt-[5vh]">
+                <div className="bg-white mr-[2vw]">
+                  <ShoppingBagIcon />
+                  Shopping
+                </div>
+                <div className="bg-white mr-[2vw]">
+                  <FastfoodIcon />
+                  Cafe and Restaurant
+                </div>
+                <div className="bg-white">
+                  <MedicationIcon />
+                  Healthcare
+                </div>
+              </div>
+          </div>
+          <div className="basis-3/6">
+            <h2 className="text-[24px] text-center">Recent Transactions</h2>
+            <Stack
+              align="stretch"
+              justify="center"
+              gap="md"
+            >
+              <Button variant="default">1</Button>
+              <Button variant="default">2</Button>
+              <Button variant="default">3</Button>
+            </Stack>
           </div>
         </div>
-
-        <div className="ml-[3vw]">
-          <div>
-            <h1 className="text-[48px]">Dashboard</h1>
-            <p>{date()}</p>
-          </div>
-
-          <div className="bg-white pr-[20vw] pl-[2vw] pt-[2vh] pb-[2vh] rounded-[15px] mt-[4vh]">
-            <h2 className="text-[30px]">Your balance:</h2>
-            <h1 className="text-[30px]">
-              ${balance !== null ? balance : "Loading..."}
-            </h1>
-          </div>
-
-          <div className="flex mt-[7vh]">
-            <div className="bg-white rounded-[15px] p-8 mr-[1vw]">
-              <select name="date" id="dates" className="items-center">
-                <option value="week">Last week</option>
-                <option value="month">Last Month</option>
-                <option value="month6">Last six month</option>
-                <option value="year">Last year</option>
-              </select>
-            </div>
-            <div className="bg-white rounded-[15px] p-8 mr-[1vw]">
-              <h3>Transaction</h3>
-              <p>{trans !== null ? trans : "Loading..."}</p>
-            </div>
-            <div className="bg-white rounded-[15px] p-8">
-              <h3>Total Spent</h3>
-              <p>{total !== null ? total : "Loading..."}</p>
-            </div>
-          </div>
-
-          <div className="flex mt-[5vh]">
-            <div className="bg-white mr-[2vw]">
-              <ShoppingBagIcon />
-              Shopping
-            </div>
-            <div className="bg-white mr-[2vw]">
-              <FastfoodIcon />
-              Cafe and Restaurant
-            </div>
-            <div className="bg-white">
-              <MedicationIcon />
-              Healthcare
-            </div>
-          </div>
-        </div>
-
-        <Modal opened={opened} onClose={setOpened.close} title="New Transaction">
+        <Modal
+          opened={opened}
+          onClose={setOpened.close}
+          title="New Transaction"
+        >
           <LoadingOverlay
             visible={visible}
             zIndex={1000}
@@ -306,7 +356,11 @@ export default function Dashboard() {
 
           <Stack align="stretch" justify="center" gap="xs">
             <Input.Wrapper required={true} label="Transaction Name">
-              <Input required={true} value={transactionName} onChange={setTransactionName} />
+              <Input
+                required={true}
+                value={transactionName}
+                onChange={setTransactionName}
+              />
             </Input.Wrapper>
 
             <NumberInput
@@ -314,14 +368,11 @@ export default function Dashboard() {
               placeholder="Dollars"
               prefix="$"
               value={cost}
-							required={true}
+              required={true}
               onChange={setCost}
             />
-            <Combobox
-              store={combobox}
-              onOptionSubmit={handleValueSelect}
-            >
-						<Combobox.Header>Tags</Combobox.Header>
+            <Combobox store={combobox} onOptionSubmit={handleValueSelect}>
+              <Combobox.Header>Tags</Combobox.Header>
               <Combobox.DropdownTarget>
                 <PillsInput onClick={() => combobox.openDropdown()}>
                   <Pill.Group>
@@ -367,21 +418,19 @@ export default function Dashboard() {
               onChange={setDate}
               label="Date"
               placeholder="Date"
-							required={true}
+              required={true}
             />
             <Button onClick={submitNewTransaction}>Add Transaction</Button>
           </Stack>
         </Modal>
-				<div className="absolute bottom-10 right-10 rounded-[100%] pd-[26px] bg-slate-400 transform scale-300 hover:scale-150 transition-transform">
+        <div className="absolute bottom-10 right-10 rounded-[100%] pd-[26px] bg-slate-400 transform scale-300 hover:scale-150 transition-transform">
           <button onClick={setOpened.open}>
             <AddIcon style={{ fontSize: 36 }} />
           </button>
         </div>
-				
       </div>
 
-			<Notifications />
-
+      <Notifications />
     </MantineProvider>
   );
 }
